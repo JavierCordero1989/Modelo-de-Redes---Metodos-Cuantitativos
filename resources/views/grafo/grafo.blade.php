5a5543ac68309a8f371f5b965f30cbfcb0bfdb13
JavierCL <javicordero20022016@gmail.com>
2018-06-24 17:44:21 -0600

    Vistas
    Cambios en las vistas de grafo y de proyectos

--- resources/views/grafo/grafo.blade.php
+++ resources/views/grafo/grafo.blade.php
@@ -4,8 +4,8 @@
     var nombres_nodos = <?php echo json_encode($nombres); ?>; //Nombres de los nodos obtenidos e la BD
 
     //Relaciones desde y hasta de los nodos para realizar las conexiones
-    var from = <?php echo json_encode($conexiones['ids_from']); ?>;
-    var to = <?php echo json_encode($conexiones['ids_to']); ?>;
+    var _from = <?php echo json_encode($conexiones['ids_from']); ?>;
+    var _to = <?php echo json_encode($conexiones['ids_to']); ?>;
 </script>
 <script src="{{ asset('js/vis.min.js') }}"></script>
 <!-- <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script> -->
@@ -14,58 +14,86 @@
 <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
 
-<div class="col-sm-6 col-md-offset-3" id="app">
-    <div class="col-sm-12" style="padding: 25px 0px 25px 0px;">
-        <ul>
-            <div class="col-sm-12">
-                ID:<input v-model="id_nodo" class="form-control" readonly="readonly">
-                Nombre: <input v-model="nombre" class="form-control" placeholder="Ingrese el nombre">
-                <button @click="addNodes(id_nodo, nombre)" class="btn btn-success">Agregar Nodo</button>
+<!-- Caja que contiene los campos para mantenimientos -->
+<div class="col-sm-12" id="app">
+    <!-- Caja izquierda para el mantenimiento de los nodos -->
+    <div class="col-md-6">
+        <div class="box box-warning">
+            <div class="box-header with-border">
+                <h3 class="box-title">Mantenimiento para los nodos</h3>
             </div>
-        </ul>
-    </div>
 
-    <div class="col-sm-12" style="padding: 25px 0px 25px 0px;">
-        <ul>
-            <div class="col-sm-12">
-                Desde el nodo: <input v-model="from" class="form-control" placeholder="Desde">
-                Hasta el nodo: <input v-model="to" class="form-control" placeholder="Hasta">
-                <button @click="add(from, to)" class="btn btn-success">Agregar Arista</button>
+            <div class="box-body">
+                <div class="form-group">
+                    <label for="">ID:</label>
+                    <input v-model="id_nodo" class="form-control" readonly="readonly" placeholder="Aqui el ID del nodo">
+                </div>
+                <div class="form-group">
+                    <label for="">Nombre:</label>
+                    <input v-model="nombre" class="form-control" placeholder="Ingrese el nombre">
+                </div>
             </div>
-        </ul>
+            <div class="box-footer">
+                <button @click="addNodes(id_nodo, nombre)" class="btn btn-success pull-left">Agregar Nodo</button>
+                <button class="btn btn-danger pull-right">Eliminar Nodo</button>
+            </div>
+        </div>
     </div>
+    <!-- Termina la caja izquierda -->
 
+    <!-- Caja derecha para el mantenimiento de las aristas -->
+    <div class="col-md-6">
+        <div class="box box-warning">
+            <div class="box-header with-border">
+                <h3 class="box-title">Mantenimiento para las aristas</h3>
+            </div>
+
+            <div class="box-body">
+                <div class="form-group">
+                    <label for="">Desde el nodo: @{{nombreDelNodoDesde}}</label>
+                    <input type="number" v-model="from" class="form-control" placeholder="Desde">
+                </div>
+                <div class="form-group">
+                    <label for="">Hasta el nodo: @{{nombreDelNodoHasta}}</label>
+                    <input type="number" v-model="to" class="form-control" placeholder="Hasta">
+                </div>
+            </div>
+            <div class="box-footer">
+                <button @click="addEdges(from, to)" class="btn btn-success pull-left">Agregar Arista</button>
+                <button class="btn btn-danger pull-right">Eliminar Arista</button>
+            </div>
+        </div>
+    </div>
+    <!-- Termina la caja derecha -->
 </div>
+<!-- Termina la caja que contiene los mantenimientos -->
 
 <!-- Grafico con nodos en imagen -->
 <div class="col-sm-10 col-md-offset-1" id="grafo_img" style="height: 400px; border: 1px solid black;"></div>
 
 <script>
-    
-    // create people.
-    // value corresponds with the age of the person
-    // var imagen = 'https://cdn.pixabay.com/photo/2017/01/13/01/22/hotel-1976102_960_720.png';
-    var imagen = 'http://resizer.123inventatuweb.com/acens110017/image/icono-edificios.png?w=960';
-    // var ids_nodos = <?php //echo json_encode($idsNodos); ?>; //ID's obtenidos de la BD
-    // var nombres_nodos = <?php //echo json_encode($nombresNodos); ?>; //Nombres de los nodos obtenidos e la BD
+    //Arreglo vacio para los cambios.
+    var id_nodos_nuevos = [];
+    var nombre_nodos_nuevos = [];
+    var id_from_nuevo = [];
+    var id_to_nuevo = [];
+
+    //Imagen para lso nodos
+    var imagen = "{{ asset('img/icono-edificios.png') }}";
 
     nodes = []; //nodos para agregar a la red
 
     //Se agregan los nodos de la BD a la red
     for (index = 0; index < ids_nodos.length; index++) {
-        nodes.push({id:ids_nodos[index].id, shape:'circularImage', image:imagen, label:(ids_nodos[index].id + ":" + nombres_nodos[index].nombre_nodo)});  ;
+        nodes.push({id:ids_nodos[index], shape:'circularImage', image:imagen, label:(ids_nodos[index] + ":" + nombres_nodos[index])});  ;
     }
-
-    //Relaciones desde y hasta de los nodos para realizar las conexiones
-    // var from = <?php //echo json_encode($var_from); ?>;
-    // var to = <?php //echo json_encode($var_to); ?>;
 
     //Aristas de la red, conexiones entre nodos
     edges = [];
 
     //Se agregan las conexiones de la BD a la red
-    for (index = 0; index < from.length; index++) {
-        edges.push({from:from[index].id_from, to:to[index].id_to});
+    for (index = 0; index < _from.length; index++) {
+        edges.push({from:_from[index], to:_to[index]});
     }
 
     // create a network
@@ -107,42 +135,73 @@ var app = new Vue({
             from: '',
             to: '',
             edges: edges,
-            nodes: nodes
+            nodes: nodes,
+            nombresNodos: nombres_nodos,
+            nombreDelNodoDesde: '',
+            nombreDelNodoHasta: ''
         }
     },
     methods: {
-        add() {
-            this.edges.push({from:'', to:''})
-        },
-        add(from, to){
-            this.edges.push({from:this.from, to:this.to})
-            network.setData({nodes: this.nodes, edges: this.edges})
-            this.from = ''
-            this.to = ''
-        },
-        addNodes(id){
-            this.nodes.push({id:id, shape: 'circularImage', image: imagen})
+        addEdges(from, to){
+            this.edges.push({from:this.from, to:this.to}) //Agrega la conexion entre dos nodos
+
+            _from.push(parseInt(from)) //Agrega el nuevo ID desde al arreglo
+            id_from_nuevo.push(parseInt(from)) //Agrega el nuevo ID desde al arreglo para conservar los cambios
+            _to.push(parseInt(to)) //Agrega el nuevo ID hasta al arreglo
+            id_to_nuevo.push(parseInt(to)) //Agrega el nuevo ID hasta al arreglo para conservar los cambios
+
+            network.setData({nodes: this.nodes, edges: this.edges}) //Actualiza el grafo
+            this.from = '' //Vacia el campo para la arista desde
+            this.to = '' //Vacia el campo para la arista hasta
         },
         addNodes(id, nombre){
             this.nodes.push({id:id, shape: 'circularImage', image: imagen, label:(id+":"+nombre)})
-            network.setData({nodes: this.nodes, edges: this.edges})
-            this.id_nodo = parseInt(nodes[nodes.length-1].id)+1
-            this.nombre = ''
+
+            ids_nodos.push(id) //Agrega el id del nuevo nodo a los id ya guardados
+            nombres_nodos.push(nombre) //Agrega el nombre del nuevo nodo a los ya guardados
+
+            id_nodos_nuevos.push(id) //Agrega el nuevo ID al arreglo para mantener los nuevos cambios
+            nombre_nodos_nuevos.push(nombre) // Agrega el nuevo nombre al arreglo para mantener los nuevos cambios
+
+            network.setData({nodes: this.nodes, edges: this.edges}) //Actualiza el grafo
+
+            this.id_nodo = parseInt(nodes[nodes.length-1].id)+1 //Actualiza el ID siguiente
+            this.nombre = '' //Pone en blanco el campo del nombre del nodo
         },
         update(){
             network.setData({nodes: this.nodes, edges: this.edges})
         }
     },
-    // watch: {
-    //     edges: {
-    //         handler(){
-    //             this.update()
-    //         },
-    //         deep: true
-    //     },
-    //     nodes() {
-    //         this.update()
-    //     }
-    // }
+    watch: {
+        from() {
+            // Va a comparar que lo que se ingresa en el campo de texto sea un dato numerico, y que ademas, no 
+            // sea mayor que la cantidad de elementos del arreglo de nodos.
+            if(this.from === '' || parseInt(this.from) > nodes.length) {
+                this.nombreDelNodoDesde = '';
+            }
+            else {
+                this.nombreDelNodoDesde = this.nombresNodos[parseInt(this.from)-1];
+            }
+        },
+        to() {
+            // Va a comparar que lo que se ingresa en el campo de texto sea un dato numerico, y que ademas, no 
+            // sea mayor que la cantidad de elementos del arreglo de nodos.
+            if(this.to === '' || parseInt(this.to) > nodes.length) {
+                this.nombreDelNodoHasta = '';
+            }
+            else {
+                this.nombreDelNodoHasta = this.nombresNodos[parseInt(this.to)-1];
+            }
+        }
+        // edges: {
+        //     handler(){
+        //         this.update()
+        //     },
+        //     deep: true
+        // },
+        // nodes() {
+        //     this.update()
+        // }
+    }
 })
 </script>
