cdefa7df08688b9508e54ddf4c000031f9d646ce
JavierCL <javicordero20022016@gmail.com>
2018-06-25 21:28:01 -0600

    Vista
    Ajustes a la interfaz visual del grafo

--- resources/views/grafo/grafo.blade.php
+++ resources/views/grafo/grafo.blade.php
@@ -21,6 +21,11 @@
         <div class="box box-warning">
             <div class="box-header with-border">
                 <h3 class="box-title">Mantenimiento para los nodos</h3>
+                <div class="box-tools pull-right">
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-info btn-xs" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                    </div>
+                </div>
             </div>
 
             <div class="box-body">
@@ -35,7 +40,7 @@
             </div>
             <div class="box-footer">
                 <button @click="addNodes(id_nodo, nombre)" class="btn btn-success pull-left">Agregar Nodo</button>
-                <button id="btn_eliminar_nodo" class="btn btn-danger pull-right hide">Eliminar Nodo</button>
+                <button id="btn_eliminar_nodo" @click="removeSelectedNode()" class="btn btn-danger pull-right hide">Eliminar Nodo</button>
             </div>
         </div>
     </div>
@@ -46,6 +51,11 @@
         <div class="box box-warning">
             <div class="box-header with-border">
                 <h3 class="box-title">Mantenimiento para las aristas</h3>
+                <div class="box-tools pull-right">
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-info btn-xs" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                    </div>
+                </div>
             </div>
 
             <div class="box-body">
@@ -72,6 +82,12 @@
 <div class="col-sm-10 col-md-offset-1" id="grafo_img" style="height: 400px; border: 1px solid black;"></div>
 
 <script>
+
+    //Actualiza el grafo
+    function update(){
+        network.setData({nodes: this.nodes, edges: this.edges})
+    }
+
     //Arreglo vacio para los cambios.
     var id_nodos_nuevos = [];
     var nombre_nodos_nuevos = [];
@@ -135,7 +151,7 @@
 
         id_arista = properties.edges; //Guarda los ids de las aristas seleccionadas
         id_nodo = properties.nodes; //Guarda los ids de los nodos seleccionados
-
+        
         if(properties.nodes == '') {
             //Oculta el boton de borrar nodo
             $('#btn_eliminar_nodo').removeClass("show");
@@ -143,13 +159,22 @@
         else {
             //Muestra el boton de borrar nodo
             $('#btn_eliminar_nodo').addClass("show");
+
+            //Mostrar el nodo, asi como las aristas seleccionadas
+            console.log('Nodo: ' + id_nodo);
+            console.log('Aristas ' + id_arista);
         }
 
         if(properties.edges == '') {
             $('#btn_eliminar_arista').removeClass("show");
         }
         else {
-            $('#btn_eliminar_arista').addClass("show");
+            if(properties.nodes == '') {
+                $('#btn_eliminar_arista').addClass("show");
+            }
+            else {
+                $('#btn_eliminar_arista').removeClass("show");
+            }
         }
     });
 
@@ -171,34 +196,72 @@ var app = new Vue({
     },
     methods: {
         addEdges(from, to){
-            this.edges.push({from:this.from, to:this.to}) //Agrega la conexion entre dos nodos
+            if(from == '' || to == '') {
+                console.log('No agrega ninguna arista porque esta el FROM o el TO estan en blanco...');
+            }
+            else {
+                this.edges.push({from:this.from, to:this.to}) //Agrega la conexion entre dos nodos
 
-            _from.push(parseInt(from)) //Agrega el nuevo ID desde al arreglo
-            id_from_nuevo.push(parseInt(from)) //Agrega el nuevo ID desde al arreglo para conservar los cambios
-            _to.push(parseInt(to)) //Agrega el nuevo ID hasta al arreglo
-            id_to_nuevo.push(parseInt(to)) //Agrega el nuevo ID hasta al arreglo para conservar los cambios
+                _from.push(parseInt(from)) //Agrega el nuevo ID desde al arreglo
+                id_from_nuevo.push(parseInt(from)) //Agrega el nuevo ID desde al arreglo para conservar los cambios
+                _to.push(parseInt(to)) //Agrega el nuevo ID hasta al arreglo
+                id_to_nuevo.push(parseInt(to)) //Agrega el nuevo ID hasta al arreglo para conservar los cambios
 
-            network.setData({nodes: this.nodes, edges: this.edges}) //Actualiza el grafo
-            this.from = '' //Vacia el campo para la arista desde
-            this.to = '' //Vacia el campo para la arista hasta
+                // network.setData({nodes: this.nodes, edges: this.edges}) //Actualiza el grafo
+                update();
+                this.from = '' //Vacia el campo para la arista desde
+                this.to = '' //Vacia el campo para la arista hasta
+            }
         },
         addNodes(id, nombre){
-            this.nodes.push({id:id, shape: 'circularImage', image: imagen, label:(id+":"+nombre)})
+            if( nombre == '') {
+                console.log('No agrega el nodo, ya que el nombre esta vacio...');
+            }
+            else {
+                this.nodes.push({id:id, shape: 'circularImage', image: imagen, label:(id+":"+nombre)})
 
-            ids_nodos.push(id) //Agrega el id del nuevo nodo a los id ya guardados
-            nombres_nodos.push(nombre) //Agrega el nombre del nuevo nodo a los ya guardados
+                ids_nodos.push(id) //Agrega el id del nuevo nodo a los id ya guardados
+                nombres_nodos.push(nombre) //Agrega el nombre del nuevo nodo a los ya guardados
 
-            id_nodos_nuevos.push(id) //Agrega el nuevo ID al arreglo para mantener los nuevos cambios
-            nombre_nodos_nuevos.push(nombre) // Agrega el nuevo nombre al arreglo para mantener los nuevos cambios
+                id_nodos_nuevos.push(id) //Agrega el nuevo ID al arreglo para mantener los nuevos cambios
+                nombre_nodos_nuevos.push(nombre) // Agrega el nuevo nombre al arreglo para mantener los nuevos cambios
 
-            network.setData({nodes: this.nodes, edges: this.edges}) //Actualiza el grafo
+                // network.setData({nodes: this.nodes, edges: this.edges}) //Actualiza el grafo
+                update();
 
-            this.id_nodo = parseInt(nodes[nodes.length-1].id)+1 //Actualiza el ID siguiente
-            this.nombre = '' //Pone en blanco el campo del nombre del nodo
+                this.id_nodo = parseInt(nodes[nodes.length-1].id)+1 //Actualiza el ID siguiente
+                this.nombre = '' //Pone en blanco el campo del nombre del nodo
+            }
         },
-        update(){
-            network.setData({nodes: this.nodes, edges: this.edges})
-        }
+        // removeSelectedNode() {
+        //     console.log('Eliminando Nodos...');
+        //     var findIndex = 0;
+
+        //     //Elimina todas las aristas conectadas al nodo del arreglo edges
+        //     for(i=0; i<id_arista.length; i++) {
+        //         for(j=0; j<edges.length; j++) {
+        //             if(edges[j].id == id_arista[i]) {
+        //                 findIndex = j;
+        //                 break;
+        //             }
+        //         }
+        //         console.log('findIndex = ' + findIndex);
+        //         edges.splice(findIndex, 1);
+        //     }
+
+        //     for(i=0; i<id_nodo.length; i++) {
+        //         for(j=0; j<nodes.length; j++) {
+        //             if(nodes[j].id == id_nodo[i]) {
+        //                 findIndex = j;
+        //                 break;
+        //             }
+        //         }
+        //         nodes.splice(findIndex, 1);
+        //     }
+        //     // network.setData({nodes: this.nodes, edges: this.edges}) //Actualiza el grafo
+        //     update();
+
+        // }
     },
     watch: {
         from() {
@@ -221,15 +284,6 @@ var app = new Vue({
                 this.nombreDelNodoHasta = this.nombresNodos[parseInt(this.to)-1];
             }
         }
-        // edges: {
-        //     handler(){
-        //         this.update()
-        //     },
-        //     deep: true
-        // },
-        // nodes() {
-        //     this.update()
-        // }
     }
 })
 </script>
